fa21a6bf5ff7e3e2428181fced62c50703768c2f
weberson <sandesweberson@gmal.com>
2023-04-02 18:35:05 -0300

    create api

---
+++ index.js
@@ -0,0 +1,91 @@
+
+const { request, response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+const cors = require('cors')
+
+
+const port = process.env.PORT || 3009     // configuraçao da porta para rodar projeto
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const users = [ ]
+
+
+
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+
+    if (index < 0) {
+
+        return response.status(404).json({ error: "User not found" })
+    }
+    request.userIndex = index
+    request.userId = id
+    next()
+}
+
+
+app.get('/users', (request, response) => {
+
+    return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+
+
+app.put('/users/:id', checkUserId, (request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updetedUser = { id , name, age }
+
+
+    users[index] = updetedUser
+
+
+    return response.json(updetedUser)
+
+    next()
+})
+
+
+
+
+app.delete('/users/:id', checkUserId, (request, response) => {
+    const index = request.userIndex
+
+
+
+    users.splice(index, 1)
+
+    return response.status(204).json()
+    next()
+})
+
+
+
+
+
+app.listen(3009, () => {
+
+    console.log(`Server started on port 3009 ${port}`)
+
+})
